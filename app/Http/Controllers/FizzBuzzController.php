da54254153441bab35bebd86da6a9fbe9a111fa6
Adith Widhiantara <172410101108@cs.unej.ac.id>
2023-08-01 16:13:36 +0700

    Update: done FizzBuzz

--- app/Http/Controllers/FizzBuzzController.php
+++ app/Http/Controllers/FizzBuzzController.php
@@ -3,30 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use function response;
 
 class FizzBuzzController extends Controller
 {
-    public function fizzbuzz(Request $request)
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function fizzbuzz(Request $request): JsonResponse
     {
         $int = 100;
 
-        if($request->filled('int')){
+        if ($request->filled('int')) {
             $int = $request->int;
         }
 
         $fizzbuzz = [];
         for ($i = 1; $i <= $int; $i++) {
             if ($i % 15 === 0) {
-                $fizzbuzz[] = 'FizzBuzz';
-            } elseif ($i % 3 === 0) {
-                $fizzbuzz[] = 'Fizz';
-            } elseif ($i % 5 === 0) {
-                $fizzbuzz[] = 'Buzz';
+                $fizzbuzz[] = 'ulartangga';
+            } else if ($i % 3 === 0) {
+                $fizzbuzz[] = 'ular';
+            } else if ($i % 5 === 0) {
+                $fizzbuzz[] = 'tangga';
             } else {
                 $fizzbuzz[] = $i;
             }
         }
 
-        return $fizzbuzz;
+        return response()
+            ->json($fizzbuzz);
     }
 }
